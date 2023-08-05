a8283e6ae1a1b4420542abe1b06fe7c89453a828
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-05 16:38:00 -0600

    Creating state structure

--- lib/presentation/blocs/notifications/notifications_state.dart
+++ lib/presentation/blocs/notifications/notifications_state.dart
@@ -1,9 +1,25 @@
 part of 'notifications_bloc.dart';
 
 class NotificationsState extends Equatable {
-  const NotificationsState();
+  const NotificationsState({
+    this.status = AuthorizationStatus.notDetermined, 
+    this.notifications = const [],
+  });
   
+  final AuthorizationStatus status;
+  // TODO: Create notifications model
+  final List<dynamic> notifications;
   
   @override
-  List<Object> get props => [];
+  List<Object> get props => [ status, notifications ];
+
+  NotificationsState copyWith({
+    AuthorizationStatus? status, 
+    List<dynamic>? notifications,
+  }) {
+    return NotificationsState(
+      status: status ?? this.status,
+      notifications: notifications ?? this.notifications,
+    );
+  }
 }
