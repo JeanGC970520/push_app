8438b062435571200231eec95e9d2e6f91d84f6f
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-05 17:04:48 -0600

    Displaying permision status and calling method to request persmissions on IconButton

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -9,11 +12,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Permisos'),
+        title: context.select(
+          ( NotificationsBloc bloc ) => Text('${bloc.state.status}'),
+        ), 
         actions: [
           IconButton(
             onPressed: () {
-              // TODO: request notication permissions
+              context.read<NotificationsBloc>()
+                .requesPermission();
             }, 
             icon: const Icon( Icons.settings )
           ),
