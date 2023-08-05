c3a57a649794df523fdfb33a42159c515becfc5a
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-05 16:39:40 -0600

    Providing the NotificationsBloc on the top of app tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'presentation/blocs/notifications/notifications_bloc.dart';
 import 'config/theme/app_theme.dart';
 import 'config/router/app_router.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NotificationsBloc(),)
+      ], 
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
