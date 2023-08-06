d11776217ac4cc86905dd7e5018d5093aeb5d9ee
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-05 22:13:43 -0600

    Creating static method to initialize Firebase

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,6 +1,9 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -11,6 +14,12 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  static Future<void> initializeFirebase() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   void requesPermission() async {
 
