98c260f6c77da81ceffcd28a2ec48b8618600770
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-08-25 16:58:44 -0600

    Adding route to navigate to DetialsScreen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@
 
 import 'package:go_router/go_router.dart';
 
+import 'package:push_app/presentation/screens/details_screen.dart';
 import 'package:push_app/presentation/screens/home_screen.dart';
 
 final appRouter =  GoRouter(
@@ -12,5 +13,9 @@ final appRouter =  GoRouter(
       builder: (context, state) => const HomeScreen(),
     ),
 
+    GoRoute(
+      path: '/push-details/:messageId',
+      builder: (context, state) => DetailsScreen( pushMessageId: state.pathParameters['messageId'] ?? '' ),
+    ),
   ]
 );
